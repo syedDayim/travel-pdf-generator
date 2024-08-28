f8a882847faaec626e40243f1bd0acfb0be74e0f
syedDayim <dayim1277@gmail.com>
2024-08-28 12:13:15 +0530

    Project Skeleton Setup

---
+++ script.js
@@ -0,0 +1,4 @@
+document.getElementById('preview-button').addEventListener('click', function() {
+    // Change the image to show a PDF preview
+    document.getElementById('image-preview').src = 'https://fakeimg.pl/600x700?text=Preview+Img';
+});
